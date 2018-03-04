8e2b42009b17bbdecaef13ba6044fb0200f41df8
Peihao Sun <phsun@sh-ln04.stanford.edu>
2018-03-03 18:22:22 -0800

    modifications

--- exposures.py
+++ exposures.py
@@ -4,7 +4,8 @@ python version of MATLAB code by Mariano Trigo
 import numpy as np
 import matplotlib.pyplot as plt
 from outgoingks import outgoingks
-from idi_str_factors import idi_str_factors
+from idi_str_factors import idi_str_factors_crys, idi_str_factors_noncrys
+from settings import basedir 
 
 ## setup
 geometry = {}
@@ -26,17 +27,6 @@ geometry['det_size_horz'] = 186.12
 K = outgoingks(geometry)
 N_pix = K.shape[0]*K.shape[1] # number of pixels
 
-## ensemble average
-N_e = 1000 # number of exposures 
-g2avg = np.zeros((N_pix, N_pix))
-for i_e in range(N_e):
-    fhkl = idi_str_factors(K)
-    Ihkl_flat = np.abs(fhkl.reshape(N_pix, 1))**2
-    g2avg = g2avg + Ihkl_flat.dot(Ihkl_flat.T)
-    if i_e % 10 == 0:
-        print "done %d exposures" % i_e
-g2avg = g2avg / N_e
-
 # calculate the Q values (= K1 - K2) 
 K_flat = K.reshape(N_pix, 1, 3)
 Q = []
@@ -44,4 +34,18 @@ for i in range(3):
     Q.append(K_flat[:,:,i] - K_flat[:,:,i].T)
 Q = np.array(Q)
 
-np.savez("results.npz", g2=g2avg, Q=Q)
+datadir = basedir + "noncrys/"
+np.save(datadir + "Q.npy", Q)
+
+## ensemble average
+N_e = 1000 # number of exposures 
+N0_e = 10 # average over every N0_e exposures
+for i_f in range(N_e/N0_e):
+    g2avg = np.zeros((N_pix, N_pix))
+    for i_e in range(N0_e):
+        fhkl = idi_str_factors_noncrys(K)
+        Ihkl_flat = np.abs(fhkl.reshape(N_pix, 1))**2
+        g2avg = g2avg + Ihkl_flat.dot(Ihkl_flat.T)
+    g2avg = 1.*g2avg / N0_e
+    np.save(datadir + "g2_%04d.npy" % (i_f), g2avg)
+    print "done %d/%d" % (i_f+1, N_e/N0_e)
---
+++ settings.py
@@ -0,0 +1,1 @@
+basedir = "/scratch/users/phsun/idi/"
